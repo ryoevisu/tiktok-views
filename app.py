55bca1564408addbe810edfe0298c24b6a5521ed
ryoevisu <ryotakashievisu@gmail.com>
2024-12-12 23:01:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -348,7 +348,7 @@ class MAIN:
 [bold red]\/_/  /__  \ \  __\   \ \  __\ \ \ \/\ \  \ \____ \  
 [bold white]  /\_____\  \ \_____\  \ \_\    \ \_____\  \/\_____\ 
 [bold white]  \/_____/   \/_____/   \/_/     \/_____/   \/_____/
-        [underline green]Free Tiktok Views - Coded by Rozhak""", width=56, style="bold bright_white")
+        [underline green]Free Tiktok Views - Coded by RyoEvisu""", width=56, style="bold bright_white")
         )
         return (True)
 
@@ -357,7 +357,7 @@ if __name__ == '__main__':
         os.system('git pull')
         subscribe_file = "Penyimpanan/Subscribe.json"
         if not os.path.exists(subscribe_file):
-            youtube_url = requests.get('https://raw.githubusercontent.com/RozhakXD/Zefoy/main/Penyimpanan/Youtube.json').json()['Link']
+            youtube_url = requests.get('https://raw.githubusercontent.com/ryoevisu/tiktok-views/refs/heads/main/Storage/Subscribe.json').json()['Link']
             os.system(f'xdg-open {youtube_url}')
             with open(subscribe_file, 'w') as w:
                 json.dump({"Status": True}, w, indent=4)
